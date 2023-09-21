dffcf5c8a4966341662c3f37ca238d07fe0c0261
woter <ve3tor89@gmail.com>
2023-09-21 22:44:19 +0300

    Создал конструктор для класса User, и создал объект класса User заполненными через консоль полями.

--- src/main/java/org/example/Dispatcher.java
+++ src/main/java/org/example/Dispatcher.java
@@ -5,8 +5,10 @@ public class Dispatcher {
         ConsoleUtils console = new ConsoleUtils();
         String nickname = console.getInput("Введите ник игрока: ");
         String login = console.getInput("Введите логин игрока: ");
-
-        System.out.println("Ник игрока: " + nickname);
-        System.out.println("Логин игрока: " + login);
+        String firstName = console.getInput("Введите имя игрока: ");
+        String lastName = console.getInput("Введите фамилию игрока: ");
+        String middleName = console.getInput("Введите отчество игрока: ");
+        User user = new User(nickname, login, firstName, lastName, middleName);
+        System.out.println("user: " + user);
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -7,4 +7,23 @@ class User extends Person {
     private String lastName;
     private String middleName;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "nickname='" + nickname + '\'' +
+                ", login='" + login + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                '}';
+    }
+
+    public User(String nickname, String login, String firstName, String lastName, String middleName) {
+        this.nickname = nickname;
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+
+    }
 }
