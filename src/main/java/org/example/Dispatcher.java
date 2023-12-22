fb7701828b79fdf085560800ea22f1602559061c
woter <ve3tor89@gmail.com>
2023-12-22 11:22:59 +0300

    Создал конструктор для класса User, и создал объект класса User заполненными через консоль полями.

--- src/main/java/org/example/Dispatcher.java
+++ src/main/java/org/example/Dispatcher.java
@@ -8,7 +8,9 @@ public class Dispatcher {
         String firstName = console.getInput("Введите имя игрока: ");
         String lastName = console.getInput("Введите фамилию игрока: ");
         String middleName = console.getInput("Введите отчество игрока: ");
+
+        System.out.println("Ник игрока: " + nickname);
+        System.out.println("Логин игрока: " + login);
         User user = new User(nickname, login, firstName, lastName, middleName);
-        System.out.println("user: " + user);
     }
 }
